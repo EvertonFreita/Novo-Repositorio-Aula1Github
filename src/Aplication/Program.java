c47fc4564ec8a68d4cc13e65891d1298d154eb1e
EvertonFreita <evertonfreitas377@gmail.com>
2019-06-30 20:00:18 -0300

    Acrescentada mensagem de aprendizagem

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -6,6 +6,7 @@ public class Program {
 	public static void main(String[] args) {
 			System.out.println("Hello world! ");
 			System.out.println("Boa noite!");
+			System.out.println("É programando que se aprende!");
 			
 	}
 
